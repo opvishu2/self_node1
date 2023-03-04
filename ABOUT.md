4c9965970a660cde6275e695543410d82b3802b1
opvishu2 <gurfire2@gmail.com>
2023-03-04 13:18:51 +0530

    port changes

---
+++ ABOUT.md
@@ -0,0 +1,7 @@
+branch wise explanation:
+    1.0: 
+        node template
+    1.1: 
+        have only one api which sends(in response) resume stored at any code's dir.
+        and is synced with branch 1.6 of FE repo https://github.com/opvishu2/self
+    
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const PORT = 5000;
+
+// app.get('/123', (req, res, next) => {
+// res.send({ abcd: "xyz" });
+// })
+app.get('/dwn', function (req, res) {
+    const file = `uploaded/resume/cvVishalM.pdf`;
+    res.download(file); // Set disposition and send it.
+});
+// Require the Routes API 
+// Create a Server and run it on the port 3000
+const server = app.listen(PORT, function () {
+    console.warn(`server is running on PORT ${PORT}`)
+    // Starting the Server at the port = PORT
+})
